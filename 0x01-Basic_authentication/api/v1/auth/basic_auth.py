8e8b5bd93aaf537b077b74b862f9f2eba7ce2ad0
Alhadji Oumate <alhadjioum0@gmail.com>
2024-06-06 15:50:41 +0100

    extract credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -29,3 +29,16 @@ class BasicAuth(Auth):
             return b64decode(base64_authorization_header).decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """extract password and email"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        email = decoded_base64_authorization_header.split(':', 1)[0]
+        password = decoded_base64_authorization_header.split(':', 1)[1]
+        return email, password
